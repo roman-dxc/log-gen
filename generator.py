2dc652906830b65664e9342681734f11a0d16054
root <root@ip-10-0-171-35.eu-west-1.compute.internal>
2019-02-19 15:44:06 +0000

    Generado fichero generador

---
+++ generator.py
@@ -0,0 +1,56 @@
+
+# +---------------+-----------------+
+# |     Level     |   Num. value    |
+# +---------------+-----------------+
+# | NONSET        |        0        |
+# | DEBUG         |       10        |
+# | INFO          |       20        |
+# | WARNING       |       30* <- DEF|
+# | ERROR         |       40*       |
+# | CRITICAL      |       50*       |
+# +---------------+-----------------+
+
+# se almacenan en el log a partir del 30 valor. Configurado a 10
+
+import logging
+import random
+from time import sleep
+
+LOG_PATH = "/home/ubuntu/logs/logejemplo.log"
+# LOG_PATH = "C:\\Users\\rbatsyatsyny\\python-workplace\\prueba.log"
+LOG_LEVEL = logging.DEBUG
+LOG_FORMAT = "%(levelname)s %(asctime)s - %(message)s"
+logging.basicConfig(
+	filename = LOG_PATH, 
+	level = LOG_LEVEL,
+	format = LOG_FORMAT
+)
+
+def insertar_en_log(logger, txt):
+	
+	nivel_log = (int(random.uniform(0,100)) % 6) * 10
+	
+	if nivel_log<=10:
+		logger.debug(txt)
+	elif nivel_log==20:
+		logger.info(txt)
+	elif nivel_log==30:
+		logger.warning(txt)
+	elif nivel_log==40:
+		logger.error(txt)
+	else:
+		logger.critical(txt)
+		
+	
+
+
+logger = logging.getLogger()
+
+for x in range(0, 100):
+	cad = " --> Entrada " + str(x) + " <-- "
+	
+	insertar_en_log(logger, cad)
+	
+	sleep(2) # segundos
+
+
