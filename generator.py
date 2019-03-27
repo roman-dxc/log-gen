269aaa79fca75cf05bceea335f1eb64a02bc9d7d
roman-dxc <42778364+roman-dxc@users.noreply.github.com>
2019-03-27 17:07:35 +0100

    actualziado generador
    insertados guiones

--- generator.py
+++ generator.py
@@ -46,7 +46,7 @@ def insertar_en_log(logger, txt):
 logger = logging.getLogger()
 
 for x in range(0, 5):
-	cad = " Memoria cache llena, el rendimiento no es optimo (" + str(x) + ")"
+	cad = " NUEVA VERSION Memoria cache llena, el rendimiento no es optimo (" + str(x) + ")"
 	
 	insertar_en_log(logger, cad)
 	
